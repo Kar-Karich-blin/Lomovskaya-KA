1a6b7b6e6b870d77e34da544d5c3918a6ea2b4da
Kara <lomovskayakarina@yandex.ru>
2024-09-23 10:46:01 +0300

    ravno ono

--- mydir/tipofail/tipofail.cpp
+++ mydir/tipofail/tipofail.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    int a, b, c;
     cout << "Gde cifri?" << '\n';
     cin >> a >> b;
+    c = a * b;
+    cout << a << "*" << b << "=" << c << '\n';
   
 }
 
