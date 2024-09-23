ef3c809370c72c5a326c58f17571088885d1895e
Kar-Karich-blin <karinalomovskaa@gmail.com>
2024-09-23 10:54:31 +0300

    Update tipofail.cpp

--- mydir/tipofail/tipofail.cpp
+++ mydir/tipofail/tipofail.cpp
@@ -7,8 +7,8 @@ int main()
     int a, b, c;
     cout << "Gde cifri?" << '\n';
     cin >> a >> b;
-    c = a * b;
-    cout << a << "*" << b << "=" << c << '\n';
+    c = a / b;
+    cout << a << "/" << b << "=" << c << '\n';
   
 }
 
